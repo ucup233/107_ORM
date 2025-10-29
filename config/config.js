993cbf29ee0a9203dcb98abc8555ec099e39e8f3
ucup233 <sufhidayat9@gmail.com>
2025-10-29 10:29:39 +0700

    Configurations

---
+++ config/config.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const development = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+};
+
+const test = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+};
+
+const production = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+};
+
+module.exports = {
+  development,
+  test,
+  production,
+};
